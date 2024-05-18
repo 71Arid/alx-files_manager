8aef6881600cd4325183bccbec8d297033404da6
71Arid <no232el@gmail.com>
2024-05-18 13:27:13 +0300

    redis.js

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    const client = redis.createClient('redis://127.0.0.1:6379')
+      .on('error', (err) => console.log(`Redis client not connected to server: ${err}`));
+    client.get = promisify(client.get).bind(client);
+    client.set = promisify(client.set).bind(client);
+    client.del = promisify(client.del).bind(client);
+    client.save();
+    this.redcli = client;
+  }
+
+  isAlive() {
+    try {
+      this.redcli.ping();
+      return true;
+    } catch (_err) {
+      return false;
+    }
+  }
+
+  async get(key) {
+    let reply;
+    try {
+      const resp = await this.redcli.get(key);
+      reply = resp;
+    } catch (err) {
+      console.error(err);
+    }
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.redcli.set(key, value, 'EX', duration);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.redcli.del(key);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
