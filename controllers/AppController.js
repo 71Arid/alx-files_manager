807001423a52f20a96bcc850824796d73d2c7664
71Arid <no232el@gmail.com>
2024-05-20 14:34:15 +0300

    appcontroller

---
+++ controllers/AppController.js
@@ -0,0 +1,19 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(_req, res) {
+    if (dbClient.isAlive() && redisClient.isAlive()) {
+      res.status(200).json({ "redis": true, "db": true })
+    }
+  }
+  static async getStats(_req, res) {
+    if (dbClient.isAlive()) {
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+      res.json({ "users": `${users}`, "files": `${files}` });
+    }
+  }
+}
+
+export default AppController;
